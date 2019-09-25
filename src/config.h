0433fe6ecccdeb64c946eaa36bdb83f4de453ff4
d3cod3 <n3m3da@d3cod3.org>
2019-09-25 17:51:00 +0200

    added bitcruncher and resonant 2 poles filter

--- src/config.h
+++ src/config.h
@@ -33,13 +33,13 @@
 #pragma mark once
 
 #define PACKAGE	"Mosaic"
-#define	VERSION	"0.2.4"
-#define VERSION_GRAPHIC "024"
+#define	VERSION	"0.2.5"
+#define VERSION_GRAPHIC "025"
 #define DESCRIPTION "Live Visual Patching Creative-Coding Platform"
 #define MOSAIC_WWW "https://mosaic.d3cod3.org/"
 #define TAGS "mosaic,openframeworks,macos,linux,windows,creative-coding,video,audio,graphics,live-coding,visual-programming,creative-coding,transmedia"
 
-#define WINDOW_TITLE "Mosaic 0.2.4"
+#define WINDOW_TITLE "Mosaic 0.2.5"
 #define WINDOW_START_WIDTH 1280
 #define WINDOW_START_HEIGHT 720
 
