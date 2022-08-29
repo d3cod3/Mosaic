d90801fcb001ee2a0f9dddd4a09123d77c97225d
d3cod3 <n3m3da@d3cod3.org>
2022-08-29 13:51:53 +0200

    fixed ffmpeg libs for OSX Monterrey

--- fixDynamicLibs_debug.sh
+++ fixDynamicLibs_debug.sh
@@ -10,4 +10,8 @@ install_name_tool -change @rpath/ffmpeg/lib/osx/libavcodec.58.dylib @executable_
 install_name_tool -change @rpath/ffmpeg/lib/osx/libavformat.58.dylib @executable_path/libavformat.58.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/ffmpeg/lib/osx/libavutil.56.dylib @executable_path/libavutil.56.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/ffmpeg/lib/osx/libswresample.3.dylib @executable_path/libswresample.3.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
+install_name_tool -change @rpath/ffmpeg/lib/osx/libavcodec.57.dylib @executable_path/libavcodec.57.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
+install_name_tool -change @rpath/ffmpeg/lib/osx/libavformat.57.dylib @executable_path/libavformat.57.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
+install_name_tool -change @rpath/ffmpeg/lib/osx/libavutil.55.dylib @executable_path/libavutil.55.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
+install_name_tool -change @rpath/ffmpeg/lib/osx/libswresample.2.dylib @executable_path/libswresample.2.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/snappy/lib/osx/libsnappy.1.dylib @executable_path/libsnappy.1.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
