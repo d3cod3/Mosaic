d22823f2e3b9abf752eb4761bc1a4b639661c6f4
d3cod3 <n3m3da@d3cod3.org>
2024-11-06 13:29:44 +0100

    added new library needed for macos

--- fixDynamicLibs_profiling.sh
+++ fixDynamicLibs_profiling.sh
@@ -6,12 +6,9 @@ APPNAME="Mosaic_profiling"
 # to check binary dependencies: otool -L $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 
 install_name_tool -change @rpath/libndi.3.dylib @executable_path/libndi.3.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
+install_name_tool -change @rpath/libopendht.2.dylib @executable_path/libopendht.2.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/ffmpeg/lib/osx/libavcodec.58.dylib @executable_path/libavcodec.58.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/ffmpeg/lib/osx/libavformat.58.dylib @executable_path/libavformat.58.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/ffmpeg/lib/osx/libavutil.56.dylib @executable_path/libavutil.56.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/ffmpeg/lib/osx/libswresample.3.dylib @executable_path/libswresample.3.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
-install_name_tool -change @rpath/ffmpeg/lib/osx/libavcodec.57.dylib @executable_path/libavcodec.57.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
-install_name_tool -change @rpath/ffmpeg/lib/osx/libavformat.57.dylib @executable_path/libavformat.57.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
-install_name_tool -change @rpath/ffmpeg/lib/osx/libavutil.55.dylib @executable_path/libavutil.55.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
-install_name_tool -change @rpath/ffmpeg/lib/osx/libswresample.2.dylib @executable_path/libswresample.2.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
 install_name_tool -change @rpath/snappy/lib/osx/libsnappy.1.dylib @executable_path/libsnappy.1.dylib $SCRIPTPATH/bin/$APPNAME.app/Contents/MacOS/$APPNAME
