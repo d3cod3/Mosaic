1d8edaad4a6dfea1128fd37cf228d04d3d8c39c5
d3cod3 <n3m3da@d3cod3.org>
2025-10-25 23:48:20 +0100

    working on 0.7.4 pre-release

--- src/config.h
+++ src/config.h
@@ -33,14 +33,15 @@
 #pragma mark once
 
 #define PACKAGE	"Mosaic"
-#define	VERSION	"0.7.3"
-#define VERSION_GRAPHIC "073"
+#define	VERSION	"0.7.4"
+#define VERSION_GRAPHIC "074"
 #define DESCRIPTION "Live Visual Patching Creative-Coding Platform"
 #define MOSAIC_WWW "https://mosaic.d3cod3.org/"
+#define MOSAIC_CREATOR "Emanuele Mazza aka n3m3da"
 #define MOSAIC_DEVELOPERS "Emanuele Mazza, Daan de Lange"
 #define TAGS "mosaic,openframeworks,macos,linux,windows,creative-coding,video,audio,graphics,live-coding,visual-programming,creative-coding,transmedia"
 
-#define WINDOW_TITLE "Mosaic 0.7.3"
+#define WINDOW_TITLE "Mosaic 0.7.4"
 #define WINDOW_START_WIDTH 1280
 #define WINDOW_START_HEIGHT 720
 
